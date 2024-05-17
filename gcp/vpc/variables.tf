c3fb10c52efbc9c3b1f141da03509c760cb3a731
Rafael Karczevski <rafael.karczevski@platformbuilders.io>
2024-05-17 13:30:51 -0400

    feat: :sparkles: adds auto_create_subnetworks option

--- gcp/vpc/variables.tf
+++ gcp/vpc/variables.tf
@@ -7,3 +7,9 @@ variable "name" {
   type        = string
   description = "VPC name"
 }
+
+variable "auto_create_subnetworks" {
+  type        = bool
+  default     = true
+  description = "Whether or not to create subnetwork for each region"
+}
