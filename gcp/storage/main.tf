9752c04e82da5340227477c2f28a3d5f13071467
Rafael Karczevski <rkarczevski@gmail.com>
2025-04-09 16:37:03 -0400

    feat: adicionar regras de ciclo de vida ao bucket de armazenamento

--- gcp/storage/main.tf
+++ gcp/storage/main.tf
@@ -14,4 +14,23 @@ resource "google_storage_bucket" "_" {
   versioning {
     enabled = true
   }
+
+  dynamic "lifecycle_rule" {
+    for_each = var.lifecycle_rules
+    content {
+      condition {
+        age                        = lifecycle_rule.value.condition.age
+        days_since_custom_time     = lifecycle_rule.value.condition.days_since_custom_time
+        days_since_noncurrent_time = lifecycle_rule.value.condition.days_since_noncurrent_time
+        matches_prefix             = lifecycle_rule.value.condition.matches_prefix
+        matches_storage_class      = lifecycle_rule.value.condition.matches_storage_class
+        matches_suffix            = lifecycle_rule.value.condition.matches_suffix
+        num_newer_versions        = lifecycle_rule.value.condition.num_newer_versions
+        with_state               = lifecycle_rule.value.condition.with_state
+      }
+      action {
+        type = lifecycle_rule.value.action.type
+      }
+    }
+  }
 }
