af6d729c7f4ff960d99fa416da8a0d93737607c0
Rafael Karczevski <rkarczevski@gmail.com>
2025-04-14 13:09:18 -0400

    feat: adicionar configuração de rede e saídas para instância Redis
    Este commit adiciona a configuração de uma rede VPC e um endereço global para o serviço Redis, além de estabelecer uma conexão de serviço privada. Também foi incluído um novo output para a string de autenticação da instância Redis, que é sensível.

--- gcp/redis/main.tf
+++ gcp/redis/main.tf
@@ -1,4 +1,28 @@
+data "google_compute_network" "network" {
+  name = var.authorized_network
+}
+
+resource "google_compute_global_address" "service_range" {
+  name          = "${var.name}-redis-service-range"
+  project       = var.project_id
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = data.google_compute_network.network.id
+}
+
+resource "google_service_networking_connection" "private_service_connection" {
+  network                 = data.google_compute_network.network.id
+  service                 = "servicenetworking.googleapis.com"
+  update_on_creation_fail = true
+  reserved_peering_ranges = [google_compute_global_address.service_range.name]
+}
+
 resource "google_redis_instance" "cache" {
+  depends_on = [
+    google_service_networking_connection.private_service_connection,
+    google_compute_global_address.service_range
+  ]
   name                    = var.name
   project                 = var.project_id
   tier                    = var.tier
